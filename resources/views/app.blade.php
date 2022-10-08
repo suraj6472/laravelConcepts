7fb719f4e273d65c8d17609b0a17c8b3df5afadd
Suraj Kumar <suraj6472@gmail.com>
2022-10-08 17:09:21 +0530

    Code Splitting and Dynamic Imports

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" integrity="sha512-wnea99uKIC3TJF7v4eKk4Y+lMz2Mklv18+r4na2Gn1abDRPPOeef95xTzdwGD9e6zXJBteMIhZ1+68QC5byJZw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+    <script src="{{ mix('/js/manifest.js') }}" defer></script>
+    <script src="{{ mix('/js/vendor.js') }}" defer></script>
+
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @inertiaHead
   </head>
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,8 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
 .vue(3)
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]).version();
+.extract()
+.postCss('resources/css/app.css', 'public/css', [
+    //
+])
+.version();
